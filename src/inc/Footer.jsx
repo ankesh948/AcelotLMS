c0001f95cfe7b1002207348b20c548095564de64
Ankesh <ankesh@acelot.in>
2025-06-13 14:32:19 +0530

    done

--- src/inc/Footer.jsx
+++ src/inc/Footer.jsx
@@ -11,6 +11,137 @@ import mepse from "../assets/images/mepse.png";
 const Footer = () => {
   return (
     <>
+        <section className='footer footer_mobile d-none'>
+        <Container>
+          <Row className='g-0 justify-content-center'>
+            <Col lg={12} xs={12}>
+              <div className='logo-footer'>
+                <Link to='/' className='d-flex justify-content-center'>
+                  <Image
+                    src={logo}
+                    className='img-fluid logo-footer-logo'
+                    alt='Acelot'
+                  />
+                </Link>
+
+                 <ul className='socialbx'>
+                  <li>
+                    <Link to='https://www.facebook.com/acelotacademy/' target='_blank'>
+                      <i className='bx bxl-facebook-circle'></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='https://twitter.com/Acelot_Academy?s=09' target='_blank'>
+                      <i className='bx bxl-twitter'></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='https://www.linkedin.com/company/acelot-academy' target='_blank'>
+                      <i className='bx bxl-linkedin'></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='https://youtube.com/channel/UCTVGTNSJpmkFWG9-ZGQkqKA' target='_blank'>
+                      <i className='bx bxl-youtube'></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='https://instagram.com/acelot.academy?utm_medium=copy_link' target='_blank'>
+                      <i className='bx bxl-instagram'></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='https://wa.me/+919999211110' target='_blank'>
+                      <i className='bx bxl-whatsapp'></i>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+
+
+            <Col lg={10} xs={12}>
+              <div className='footer_bx2'>
+                <h3 className='text-center mt-4'>Quick Links:</h3>
+                <ul>
+                  <li><Link to='/'>Home</Link></li>
+                  <li><Link to='/about-us'>About Us</Link></li>
+                  <li><Link to='/blog'>Blog</Link></li>
+                  <li><Link to='/contact-us'>Contact Us</Link></li>
+                  <li><Link to='/privacy-policy'>Privacy Policy</Link></li>
+                  <li><Link to='/onboarding-training'>Onboarding Training</Link></li>
+                  <li><Link to='/product-training'>Product Training</Link></li>
+                  <li><Link to='/remote-workforce-training'>Remote Workforce Training</Link></li>
+                  <li><Link to='/sales-training'>Sales Training </Link></li>
+                  <li><Link to='/compliance-training'>Compliance Training </Link></li>
+                  <li><Link to='/lms-platform'>LMS Platform</Link></li>
+                  <li><Link to='/corporate-training'>Corporate Training</Link></li>
+                </ul>
+              </div>
+            </Col>
+
+            <Col lg={12}  xs={12}>
+              
+                 <div className='startupbx d-flex flex-wrap gap-4 mt-5'>
+                  <div>
+                    <img src={msme} className='' alt="" />
+                  </div>
+                  <div>
+                    <img src={dpiit} className='' alt="" />
+                  </div>
+                   <div>
+                    <img src={mepse} className='' alt="" />
+                  </div>
+                  {/*<div className=''>
+                    <img src={certification1} className='' alt="" />
+                  </div>
+                  <div>
+                    <img src={certification2} className='' alt="" />
+                  </div> */}
+                </div>
+              
+            </Col>
+
+          </Row>
+        </Container>
+
+        <div className='bottom-bar'>
+          <Container>
+            <Row>
+              <Col lg={12} className='text-center'>
+
+                <h3 className='mt-4 fs-4 mb-2 fw-600'>Address : </h3>
+                <p className='pe-5 mb-5'>90 B Delhi – Jaipur Expressway, Udyog Vihar, Sector 18, Gurugram – 122018 ,Haryana, India</p>
+
+
+
+                <p className='mb-0 cont'>Copyright © 2025 Acelot. All Rights Reserved.</p>
+              </Col>
+              {/* <Col lg={5} className='text-lg-end text-center'>
+                <p className='mb-0 cont'><span>Build With</span> <i className='bx bxs-heart text-danger sdsd fs-3'></i>  <span> In India</span></p>
+              </Col> */}
+            </Row>
+          </Container>
+        </div>
+      </section >
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       <section className='footer'>
         <Container>
           <Row className='g-0'>
--- src/pages/Request_demo.jsx
+++ src/pages/Request_demo.jsx
@@ -3,57 +3,78 @@ import { Button, Col, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Home_section_two from '../components/Home_section_two'
 import Home_section_six from '../components/Home_section_six'
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+
+
 
 function Request_demo() {
+
+
+    useEffect(() => {
+        (async function () {
+            const cal = await getCalApi({ "namespace": "lets-connect" });
+            cal("ui", { "theme": "light", "cssVarsPerTheme": { "light": { "cal-brand": "#326fe8" }, "dark": { "cal-brand": "#1e2b71" } }, "hideEventTypeDetails": false, "layout": "month_view" });
+        })();
+    }, [])
+
+
     return (
         <>
+            <section className='banner_section_demo'>
+                <Container>
+                    <Row>
+                        <Col lg={12}>
+                            <div className='demo_section_banner'>
+                                <h1 className=''>Book FREE Live Demo</h1>
+                                <p className='slide_description'>Meet our Academic Expert to see how online schooling is right fit for your child.</p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
             <section className='requestdemosection'>
                 <Container>
                     <Row className='g-lg-5'>
                         <Col lg={6}>
-                            <h1 className='slide_head'>Ready to take a closer look at Acelot?</h1>
+                            <h1 className='lead5'>In this Zoom Meeting you will know about :</h1>
+                            <ul class="Meeting_bbx">
+                                <li class="">
+                                   School Recognition and Approvals
+                                </li>
+                                <li class="">
+                                    Benefits of American Curriculum
+                                </li>
+                                <li class="">
+                                   List of Courses Available
+                                </li>
+                                <li class="">
+                                    Best Learning Method for Your Child
+                                </li>
+                                <li class="">
+                                   Examination, Transcript and US High School Diploma
+                                </li>
+                                <li class="">
+                                   College &amp; University Support
+                                </li>
+                                <li class="">
+                                    Easy School Fee Options
+                                </li>
+                            </ul>
                         </Col>
                         <Col lg={6}>
-                            <form action="" className='formforDemo'>
-                                <div className="row">
-                                    <div className='col-6'>
-                                        <input type='text' className='form-control' placeholder='First name*' />
-                                    </div>
-                                    <div className='col-6'>
-                                        <input type='text' className='form-control' placeholder='Last name*' />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className='col-6'>
-                                        <input type='text' className='form-control' placeholder='Work Email*' />
-                                    </div>
-                                    <div className='col-6'>
-                                        <input type='text' className='form-control' placeholder='Phone*' />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className='col-12'>
-                                        <input type='text' className='form-control' placeholder='Company Name*' />
-                                    </div>
-                                    <div className='col-12'>
-                                        <input type='text' className='form-control' placeholder='Location*' />
-                                    </div>
-
-                                    <div className="col-lg-12">
-                                       <Link type='submit' to={''}  class="btn_four mt-4 btn btn-primary">
-                                       Book a Demo
-                                       </Link>
-                                    </div>
-                                </div>
-                            </form>
+                            <Cal namespace="lets-connect"
+                                calLink="sanyam-sharma/lets-connect"
+                                style={{ width: "100%", height: "100%", overflow: "scroll" }}
+                                config={{ "layout": "month_view", "theme": "light" }}
+                            />
                         </Col>
                     </Row>
                 </Container>
             </section>
 
             <Home_section_two />
-            <Home_section_six />
-            
+
         </>
     )
 }
